26b551fb29803781d0f3e6d8030f7918665631a2
Asad Ather <asad_ather@hotmail.com>
2022-01-31 23:45:24 -0500

    initial

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "AppstentUI",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +21,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "AppstentUI",
-            dependencies: []),
-        .testTarget(
-            name: "AppstentUITests",
-            dependencies: ["AppstentUI"]),
+            path: "./Sources/AppstentUI.xcframework"
+        ),
     ]
 )
--- Sources/AppstentUI/AppstentUI.swift
+++
@@ -1,6 +0,0 @@
-public struct AppstentUI {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
