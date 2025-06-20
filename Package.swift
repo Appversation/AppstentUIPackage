2617148b70a93cb6d9e0b2c3577a71dd99db7dda
Asad Ather <asad_ather@hotmail.com>
2025-06-20 16:54:49 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AppstentUI",
-            path: "./Sources/AppstentUI.xcframework",
+            path: "./Sources",
             dependencies: [
               .product(name: "Lottie", package: "lottie-spm")
             ],
