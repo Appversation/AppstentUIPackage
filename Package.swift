57bf85527ab7694b949e61ce004df5f3e7484d58
Asad Ather <asad_ather@hotmail.com>
2025-06-20 21:03:58 -0400

    Reverting package to binary target and removing Lottie

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "AppstentUIPackage",
+    name: "AppstentUI",
     platforms: [
         .iOS(.v18), .macOS(.v15)
     ],
@@ -16,19 +16,15 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.5.2")
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
-    targets: [
+    targets: [Add commentMore actions
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
         .target(
             name: "AppstentUI",
-            dependencies: [
-              .product(name: "Lottie", package: "lottie-spm")
-            ],
-            linkerSettings: [
-                .linkedFramework("AppstentUI")
-            ]
+            path: "./Sources/AppstentUI.xcframework"
         ),
     ]
 )
