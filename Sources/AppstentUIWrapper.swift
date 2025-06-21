12ef764374b94f914a0ea04876da7e1a9195219e
Anka <runner@Mac-1750462873540.local>
2025-06-21 00:55:28 +0000

    New AppstentUI version 0.0.180

--- Sources/AppstentUIWrapper.swift
+++
@@ -1,9 +0,0 @@
-import Lottie
-
-public struct AppstentUIWrapper {
-    public static func LottieContentView() -> LottieAnimationView {
-        let view = LottieAnimationView(name: "example")
-        view.play()
-        return view
-    }
-}
